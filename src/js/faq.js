1954e4b7ce337a52e3aaa5f5eb1f6f5b68f9068b
komar1811 <ostap.komar7@gmail.com>
2026-06-14 20:15:25 +0400

    animation logic using js applied

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,2 +1,19 @@
-// remove this code
-console.log('FAQ');
+const faqDetails = document.querySelectorAll('.faq-details');
+
+faqDetails.forEach(details => {
+  const btn = details.querySelector('.faq-question');
+
+  btn.addEventListener('click', () => {
+    const isOpen = details.classList.contains('is-open');
+
+    faqDetails.forEach(d => {
+      d.classList.remove('is-open');
+      d.querySelector('.faq-question').setAttribute('aria-expanded', 'false');
+    });
+
+    if (!isOpen) {
+      details.classList.add('is-open');
+      btn.setAttribute('aria-expanded', 'true');
+    }
+  });
+});
